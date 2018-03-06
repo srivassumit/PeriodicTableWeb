c7dfab969e3335dbee0c7c26720158a5574969df
Sumit Srivastava <srivassumit@gmail.com>
2018-03-05 22:40:45 -0500

    initial commit

---
+++ www/js/root.js
@@ -0,0 +1,17 @@
+angular.module('root', [])
+    .controller("index", ["$scope", function ($scope) {
+        $scope.title = "Periodic Table";
+        
+        $scope.details = false;
+        
+        $scope.showDetails = function(element) {
+            console.log(element);
+            $scope.element = element;
+            $scope.details = true;
+        };
+
+        $scope.goBack = function() {
+            console.log("Back pressed");
+            $scope.details = false;
+        }
+    }]);
